38d1dd26084af51f543f3d0f4327a15c75a5ce3f
Matthew R. Becker <becker.mr@gmail.com>
2022-06-01 09:57:07 -0500

    try this

--- deep_metacal/tests/test_deep_metacal.py
+++ deep_metacal/tests/test_deep_metacal.py
@@ -268,7 +268,7 @@ def test_deep_metacal_slow_terms(skip_wide, skip_deep):
             joblib.delayed(_run_sim_pair)(seed, 12, noise_fac, 1, skip_wide, skip_deep)
             for seed in _seeds
         ]
-        outputs = joblib.Parallel(n_jobs=-1, verbose=10)(jobs)
+        outputs = joblib.Parallel(n_jobs=-1, verbose=0)(jobs)
         for res in outputs:
             if res is not None:
                 res_p.append(res[0])
